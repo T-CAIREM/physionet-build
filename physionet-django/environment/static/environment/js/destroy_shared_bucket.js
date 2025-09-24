545a30c709f2f5ad9419cee30ec7de56359f2dfd
Dawid Mularczyk <dawid.mularczyk@onet.pl>
2025-09-24 11:36:06 +0200

    feat: modern ui branch init

--- physionet-django/environment/static/environment/js/destroy_shared_bucket.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    const loadingText = "<i class='fas fa-spinner fa-pulse'></i> Destroying...";
-    $(".shared-bucket-destroy").on("click", function() {
-        $(this).html(loadingText);
-    });
-});
--- physionet-django/environment/static/environment/js/forms.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    const loadingText = "<i class='fas fa-spinner fa-pulse'></i> Loading...";
-    $(".single-submit-form").submit(function() {
-        const button = $(this).find(":submit");
-        button.html(loadingText);
-        $(this).find(':submit').prop('disabled', true);
-    });
-});
